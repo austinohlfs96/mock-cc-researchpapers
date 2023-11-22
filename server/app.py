8e29a60c1d3b050233c4d3f63f56be9c5363178e
Austin Ohlfs <ohlfsam96@gmail.com>
2023-11-21 17:08:49 -0700

    complete lab

--- server/app.py
+++ server/app.py
@@ -12,16 +12,59 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
+api = Api(app)
 db.init_app(app)
 
 @app.route('/')
 def index():
     return '<h1>Code challenge</h1>'
 
-@app.route('/research')
-def restaurants():
+class Researches(Resource):
+    def get(self):
+        research_data = [r.to_dict(rules=("-research_authors","-updated_at", "-created_at",)) for r in Research.query]
+        return research_data, 200
+    
+api.add_resource(Researches, "/research")
 
-    pass
+class ResearchById(Resource):
+    def get(self, id):
+        if research := db.session.get(Research, id):
+            return research.to_dict(only=('id', "topic", 'page_count', 'authors')), 200
+        return {"error": "Research paper not found"}, 404
+    
+    def delete(self, id):
+        if research := db.session.get(Research, id):
+            try:
+                db.session.delete(research)
+                db.session.commit()
+                return {}, 204
+            except Exception as e:
+                return {"error" : str(e)}
+        return {"error": "Research paper not found"}, 404
+    
+api.add_resource(ResearchById, "/research/<int:id>")
+
+class Authors(Resource):
+    def get(self):
+        author_data = [a.to_dict() for a in Author.query]
+        return author_data, 200
+    
+api.add_resource(Authors, "/authors")
+
+class ResearchAuthor(Resource):
+    def post(self):
+        try:
+            data=request.json
+            new_ra = ResearchAuthors(**data)
+            db.session.add(new_ra)
+            db.session.commit()
+            return new_ra.to_dict(), 200
+        except Exception as e:
+            return {"error" : [str(e)]}
+        
+api.add_resource(ResearchAuthor, '/research_author')
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,8 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, func
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -11,3 +12,76 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+
+class Research(db.Model, SerializerMixin):
+    __tablename__ = "researches"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    topic = db.Column(db.String)
+    year = db.Column(db.Integer)
+    page_count = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, onupdate=func.now())
+    
+    research_authors = db.relationship("ResearchAuthors", back_populates="research", cascade="all, delete-orphan")
+    
+    authors = association_proxy("research_authors", "author")
+    
+    serialize_rules = ("-authors.researches", "-research_authors.research")
+    
+    @validates('year')
+    def validate_year(self, _, value):
+        if not isinstance(value, int):
+            raise ValueError("Year must be a number")
+        elif not len(str(value)) is 4:
+            raise ValueError("Year must be 4 digits")
+        else:
+            return value
+    
+    def __repr__(self):
+        return f"<Researcher {self.id}: {self.topic}>"
+    
+
+class Author(db.Model, SerializerMixin):
+    __tablename__ = "authors"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    field_of_study = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, onupdate=func.now())
+    
+    #relationship
+    research_authors = db.relationship("ResearchAuthors", back_populates="author", cascade="all, delete-orphan")
+    
+    researches = association_proxy("research_authors", "research")
+    
+    serialize_only = ("id", "name", "field_of_study")
+    
+    @validates('field_of_study')
+    def validate_fos(self, _, value):
+        if not value in ["AI", "Robotics", "Machine Learning", "Vision", "Cybersecurity"]:
+            raise ValueError("F.O.S. must be different")
+        return value
+    
+    def __repr__(self):
+        return f"<Author {self.id}: {self.name}>"
+    
+class ResearchAuthors(db.Model, SerializerMixin):
+    __tablename__ = "research_authors"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey("authors.id"))
+    research_id = db.Column(db.Integer, db.ForeignKey("researches.id"))
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, onupdate=func.now())
+    
+    #relationships
+    research = db.relationship('Research', back_populates="research_authors")
+    author = db.relationship('Author', back_populates="research_authors")
+    
+    #serilzer rules
+    serialize_only = ("author",)
+    
+    def __repr__(self):
+        return f"<ResearcherAuthor {self.id}: {self.name}>"
